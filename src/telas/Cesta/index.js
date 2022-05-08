c996b5738f09e24ded6aa1a91e7de3849df945cb
Paulo <pauloh16.pdr@gmail.com>
2022-05-08 12:21:39 -0300

    Componentizar tela de cesta

---
+++ src/telas/Cesta/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import Topo from './componentes/Topo';
+import Detalhes from './componentes/Detalhes';
+
+const Cesta = () => {
+    return (
+        <>
+            <Topo />
+
+            <Detalhes />
+        </>
+    );
+};
+
+export default Cesta;
